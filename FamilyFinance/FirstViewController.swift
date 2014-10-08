47d020268c0da2f5c224e312ccc9109c74437544
Gongwei Zhang <zhgw01@gmail.com>
2014-10-08 22:01:10 +0800

    Initial Commit

---
+++ FamilyFinance/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FamilyFinance
+//
+//  Created by Zhang Gongwei on 10/8/14.
+//  Copyright (c) 2014 Zhang Gongwei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FamilyFinance/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FamilyFinance
+//
+//  Created by Zhang Gongwei on 10/8/14.
+//  Copyright (c) 2014 Zhang Gongwei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
