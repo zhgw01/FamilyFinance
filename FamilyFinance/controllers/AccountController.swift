cdecdc0b34a742211b1770c33169ce4bf880a5f2
Gongwei Zhang <zhgw01@gmail.com>
2014-10-13 21:24:40 +0800

    Refactor the structure

--- FamilyFinance/controllers/AccountController.swift
+++ FamilyFinance/controllers/AccountController.swift
@@ -8,16 +8,14 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class AccountController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
