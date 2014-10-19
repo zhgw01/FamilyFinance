9ccd6af5824ffaaf0adc022df6274317231bfd57
Gongwei Zhang <zhgw01@gmail.com>
2014-10-19 21:02:20 +0800

    add library and custom view

---
+++ FamilyFinance/Views/MonthlyView.swift
@@ -0,0 +1,13 @@
+//
+//  MonthlyView.swift
+//  FamilyFinance
+//
+//  Created by Zhang Gongwei on 10/19/14.
+//  Copyright (c) 2014 Zhang Gongwei. All rights reserved.
+//
+
+import UIKit
+
+class MonthlyView: UIView {
+
+}
