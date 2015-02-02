5e8ce058c7ca9330c4b7eebf01fc9540201505f4
zhgw01 <zhgw01@gmail.com>
2015-02-02 23:05:14 +0800

    Implement table view

---
+++ FamilyFinance/View/ReportCell.swift
@@ -0,0 +1,19 @@
+//
+//  ReportCell.swift
+//  FamilyFinance
+//
+//  Created by Gongwei on 15/2/2.
+//  Copyright (c) 2015年 Zhang Gongwei. All rights reserved.
+//
+
+import UIKit
+
+class ReportCell: UITableViewCell {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+}
--- FamilyFinance/controllers/ReportsController.swift
+++ FamilyFinance/controllers/ReportsController.swift
@@ -12,6 +12,28 @@ class ReportsController: UITableViewController {
     
     lazy var cashes = Cash.allObjects()
     
+    private let dateFormatter = NSDateFormatter()
+    private let dayFormatter = NSDateFormatter()
+    private let numberFormatter = NSNumberFormatter()
+    private let colors = [
+                        UIColor(red: 13/255.0, green: 55/255.0, blue: 88/255.0, alpha: 1.0),
+                        UIColor(red: 48/255.0, green: 116/255.0, blue: 115/255.0, alpha: 1.0),
+                        UIColor(red: 104/255.0, green: 178/255.0, blue: 136/255.0, alpha: 1.0)
+                            ]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dateFormatter.locale = NSLocale(localeIdentifier: "zh_CN")
+        dateFormatter.dateFormat = "LLLL"
+        
+        dayFormatter.locale = NSLocale(localeIdentifier: "zh_CN")
+        dayFormatter.dateFormat = "dd"
+        
+        numberFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+    }
     
     //MARK: - Table view data source
     
@@ -25,13 +47,24 @@ class ReportsController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        var cell = tableView.dequeueReusableCellWithIdentifier("ReportCellIdentifier") as UITableViewCell
+        var cell = tableView.dequeueReusableCellWithIdentifier("ReportCellIdentifier") as ReportCell
         
         let cash = cashes[UInt(indexPath.row)] as Cash
         
-        cell.textLabel?.text = cash.content
+        cell.monthLabel.text = dateFormatter.stringFromDate(cash.created)
+        cell.dayLabel.text = dayFormatter.stringFromDate(cash.created)
+        cell.categoryImageView.image = UIImage(named: cash.category.image)
+        cell.titleLabel.text = cash.title
+        cell.numberLabel.text = numberFormatter.stringFromNumber(cash.number)
+        
+        let colorIndex = indexPath.row % colors.count
+        cell.backgroundColor = colors[colorIndex]
         
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 63.0
     }
     
 }
