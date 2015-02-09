4b0cbc27cf0382ae090adf19e9bc594962f6ecaa
zhgw01 <zhgw01@gmail.com>
2015-02-09 21:42:41 +0800

    add web view

--- FamilyFinance/controllers/FinanceController.swift
+++ FamilyFinance/controllers/FinanceController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class FinanceController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let request = NSURLRequest(URL: NSURL(string: "http://localhost")!)
+        webView.loadRequest(request)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
