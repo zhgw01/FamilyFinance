0517f71a6161000c2d0bdbad864e220a590b002f
zhgw01 <zhgw01@gmail.com>
2015-02-26 14:58:00 +0800

    add back gesture

--- FamilyFinance/controllers/FinanceController.swift
+++ FamilyFinance/controllers/FinanceController.swift
@@ -12,18 +12,46 @@ class FinanceController: UIViewController {
 
     @IBOutlet weak var webView: UIWebView!
     
+    func goBack() {
+        webView.goBack()
+    }
+    
+    func goForward() {
+        webView.goForward()
+    }
+    
+    private func setupGesture() {
+        //ToDo: how to add selector with parameter
+        let backSwipe = UISwipeGestureRecognizer(target: self, action: "goBack")
+        backSwipe.numberOfTouchesRequired = 1
+        backSwipe.direction = UISwipeGestureRecognizerDirection.Right
+        backSwipe.delegate = self
+        
+        webView.addGestureRecognizer(backSwipe)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupGesture()
+        
         let path = NSBundle.mainBundle().pathForResource("local.html", ofType: nil, inDirectory: "Html")
         let url = NSURL(fileURLWithPath: path!)
-        //http://finance.sina.com.cn/fund/box/
-        //let url = NSURL(string: "http://m.howbuy.com/fund/")
         let request = NSURLRequest(URL: url!)
         webView.loadRequest(request)
     }
 
+}
 
-
+extension FinanceController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+//    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOfGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+//        return true
+//    }
 }
 
